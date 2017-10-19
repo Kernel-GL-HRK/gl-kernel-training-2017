1b101e38f2670f7d3b6531d57af820c3050a4cfc
Andriy.Khulap <andriy.khulap@globallogic.com>
2017-10-19 17:52:41 +0300

    mpu6050: Add I2C driver
    I2C driver consist of:
    
    1. probe and remove functions. Called on driver load and removal respectively.
    
    2. i2c_device_id structure. Should contain strings exact equal to devive tree.
       MODULE_DEVICE_TABLE() macro is mandatory for match with dtb. Without it
       driver will not be probed.
       static const struct i2c_device_id mpu6050_idtable [] = {
           { "gl_mpu6050", 0 },
           { }
       };
       MODULE_DEVICE_TABLE(i2c, mpu6050_idtable);
    
    3. i2c_driver structure itself.
       Important fields: probe, remove and id_table should point to items described
       above.
       static struct i2c_driver mpu6050_i2c_driver = {
           .driver = {
               .name = "gl_mpu6050",
           },
    
           .probe = mpu6050_probe ,
           .remove = mpu6050_remove,
           .id_table = mpu6050_idtable,
       };
    
    I2C driver is created using i2c_add_driver(&mpu6050_i2c_driver) function.
    It will return 0 on success and most likely is called from module_init().
    Please note that without device tree match probe function will not be called,
    but i2c_add_driver() will return OK. With device tree match, probe function
    will be called right after i2c_add_driver().
    
    I2C driver is removed with i2c_del_driver(&mpu6050_i2c_driver) function.
    If driver was created, remove function will be called.
    
    Signed-off-by: Andriy.Khulap <andriy.khulap@globallogic.com>

--- mpu6050/mpu6050.c
+++ mpu6050/mpu6050.c
@@ -2,15 +2,58 @@
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/err.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+
+static int mpu6050_probe(struct i2c_client *drv_client, const struct i2c_device_id *id)
+{
+	printk(KERN_INFO "mpu6050: i2c client address is 0x%X\n", drv_client->addr);
+	printk(KERN_INFO "mpu6050: i2c driver probed\n");
+	return 0;
+}
+
+static int mpu6050_remove(struct i2c_client *drv_client)
+{
+	printk(KERN_INFO "mpu6050: i2c driver removed\n");
+	return 0;
+}
+
+static const struct i2c_device_id mpu6050_idtable [] = {
+	{ "gl_mpu6050", 0 },
+	{ }
+};
+
+static struct i2c_driver mpu6050_i2c_driver = {
+	.driver = {
+		.name = "gl_mpu6050",
+	},
+
+	.probe = mpu6050_probe ,
+	.remove = mpu6050_remove,
+	.id_table = mpu6050_idtable,
+};
 
 static int mpu6050_init(void)
 {
+	int ret;
+
+	/* Create i2c driver */
+	ret = i2c_add_driver(&mpu6050_i2c_driver);
+	if (ret) {
+		printk(KERN_ERR "mpu6050: failed to add new i2c driver: %d\n", ret);
+		return ret;
+	}
+	printk(KERN_INFO "mpu6050: i2c driver created\n");
+
 	printk(KERN_INFO "mpu6050: module loaded\n");
 	return 0;
 }
 
 static void mpu6050_exit(void)
 {
+	i2c_del_driver(&mpu6050_i2c_driver);
+	printk(KERN_INFO "mpu6050: i2c driver deleted\n");
+
 	printk(KERN_INFO "mpu6050: module exited\n");
 }
 
